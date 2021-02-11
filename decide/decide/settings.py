a585ddebe46a6120c97ccc2ea2a4c6db41260ca8
danaremar <danaremar@alum.us.es>
2021-02-11 20:50:53 +0000

    Despliegue contínuo realizado

--- decide/decide/settings.py
+++ decide/decide/settings.py
@@ -70,7 +70,7 @@ MODULES = [
     'voting',
 ]
 
-BASEURL = 'http://localhost:8000'
+BASEURL = 'https://egc-dan3.herokuapp.com'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -180,3 +180,8 @@ if os.path.exists("config.jsonnet"):
 
 
 INSTALLED_APPS = INSTALLED_APPS + MODULES
+
+APIS = {}
+
+import django_heroku
+django_heroku.settings(locals())
